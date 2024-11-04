fe15b7f8ba06c20b6840084d8ae0b21953eb5695
Tittawat Jai-Ou <tittawat.j@arise.tech>
2024-11-04 15:06:24 +0700

    Fix close webview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 declare global {
   interface Window {
     Android?: {
-      closeWebView: () => void;
-      openExternalBrowser: (url: string) => void;
+      closeWebView: () => {};
+      openExternalBrowser: (url: string) => {};
     }
   }
 }
@@ -26,7 +26,7 @@ export default function Home() {
     }
   }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen h-full p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header
         className="flex justify-end w-full"
       >
@@ -56,6 +56,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <button
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            onClick={closeView}
           >
             Close WebView
           </button>
