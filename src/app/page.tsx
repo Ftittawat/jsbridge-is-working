bf6294d69b53400a5d4d9bee81c71eae071e8384
Tittawat Jai-Ou <tittawat.j@arise.tech>
2024-11-05 11:19:34 +0700

    Add getKeyboardHeight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,14 @@ declare global {
     Android?: {
       closeWebView: () => void
       openExternalBrowser: (url: string) => void
+      getKeyboardHeight: () => void
     }
     webkit?: {
       messageHandlers?: {
         iOSApp?: {
           closeWebView: () => void
           openExternalBrowser: (url: string) => void
+          getKeyboardHeight: () => void
         }
       }
     }
@@ -20,9 +22,11 @@ declare global {
 
 export default function Home() {
   const [inputText, setInputText] = useState("")
+
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(event.target.value)
-  };
+  }
+
   const closeView = () => {
     if (window.Android && window.Android.closeWebView) {
       window.Android.closeWebView()
@@ -30,6 +34,7 @@ export default function Home() {
       window.webkit.messageHandlers.iOSApp.closeWebView()
     }
   }
+
   const openBrowser = (url: string) => {
     if (window.Android && window.Android?.openExternalBrowser) {
       window.Android?.openExternalBrowser(url)
@@ -37,6 +42,15 @@ export default function Home() {
       window.webkit.messageHandlers.iOSApp.openExternalBrowser(url)
     }
   }
+
+  const keyboardHeight = () => {
+    if (window.Android && window.Android.getKeyboardHeight) {
+      window.Android.getKeyboardHeight()
+    } else if (window.webkit?.messageHandlers?.iOSApp) {
+      window.webkit.messageHandlers.iOSApp.getKeyboardHeight()
+    }
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen h-full p-8 pt-16 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header
@@ -49,24 +63,24 @@ export default function Home() {
           X
         </button>
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center">
+      <main className="flex flex-col gap-8 row-start-2 items-center w-full">
         {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <h1 className="font-poppins font-semibold text-white text-4xl text-end p-5 pb-2 pr-10 leading-[4rem]">
             <span className="text-4xl max-lg:text-2xl">JsBridge is Working</span>
           </h1>
         </div> */}
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 items-center flex-col sm:flex-row max-sm:w-full max-w-96">
           <input
             type="url"
-            placeholder="Enter URL"
-            className="border border-gray-300 text-black rounded px-4 py-2 text-sm sm:text-base w-full sm:w-96"
+            placeholder="https://...."
+            className="border border-gray-300 text-black rounded px-4 py-2 text-base w-full sm:w-96"
             value={inputText}
             onChange={handleInputChange}
           />
         </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <button
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-5 sm:px-6"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#ccc] text-base h-10 sm:h-12 px-5 sm:px-6"
             onClick={() => openBrowser(inputText)}
           >
             Open URL
@@ -74,23 +88,23 @@ export default function Home() {
         </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <button
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-5 sm:px-6"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#ccc] text-base h-10 sm:h-12 px-5 sm:px-6"
             onClick={() => openBrowser("https://www.google.co.th/")}
           >
             Open Google.com
           </button>
         </div>
-        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <button
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            onClick={() => openBrowser("https://www.google.co.th/")}
-          >
-            getKeyboardHeight (not working)
-          </button>
-        </div> */}
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <button
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-5 sm:px-6"
+             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#ccc] text-base h-10 sm:h-12 px-5 sm:px-6"
+             onClick={keyboardHeight}
+          >
+            getKeyboardHeight
+          </button>
+        </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <button
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#ccc] text-base h-10 sm:h-12 px-5 sm:px-6"
             onClick={closeView}
           >
             Close WebView
